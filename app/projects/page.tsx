93ea41359612c54f5f3b193f558a4e5dcc577ea6
J <jrosseruk@gmail.com>
2023-11-05 14:54:30 +0000

    removing erroneous characters

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -31,7 +31,7 @@ export default async function Project() {
           </h1>
           <p className="text-base dark:text-zinc-400 text-zinc-600 leading-relaxed">
           
-            The projects showcased form a small representative sample of J's AI, Machine Learning and Computer Vision portfolio. Unfortunately, not all projects can be released due to NDAs.
+            The projects showcased form a small representative sample of J&apos;s AI, Machine Learning and Computer Vision portfolio. Unfortunately, not all projects can be released due to NDAs.
           </p>
         </Slide>
       </header>
