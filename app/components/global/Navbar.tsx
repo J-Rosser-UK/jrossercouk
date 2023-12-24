842da23361143f469a225931a0ab4124cb55c9b8
J <jrosseruk@gmail.com>
2023-12-24 18:58:49 +0000

    New theme

--- app/components/global/Navbar.tsx
+++ app/components/global/Navbar.tsx
@@ -2,22 +2,30 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/logo.png";
-import Theme from "./Theme";
+// import Theme from "./Theme";
 import UnmountStudio from "./Unmount";
 import MobileMenu from "./MobileMenu";
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
+import SunIcon from "../../icons/SunIcon";
+import MoonIcon from "../../icons/MoonIcon";
 
 export default function Navbar() {
 
   const { systemTheme, theme, setTheme } = useTheme();
   const [hasMounted, setHasMounted] = useState(false);
   const currentTheme = theme === "system" ? systemTheme : theme;
-
+  console.log(systemTheme, theme, currentTheme)
   function toggleTheme() {
+    console.log(systemTheme, theme, currentTheme)
     return currentTheme === "light" ? setTheme("dark") : setTheme("light");
   }
   useEffect(() => setHasMounted(true), []);
+
+  if (!hasMounted)
+    return (
+      <span className="animate-pulse min-w-[28px] min-h-[28px] p-2 rounded-full dark:bg-zinc-800 bg-zinc-200 border dark:border-zinc-700 border-zinc-300"></span>
+    );
 
   const data = [
     {
@@ -62,7 +70,15 @@ export default function Navbar() {
           </nav>
 
           <div className="flex items-center gap-x-4">
-            <Theme />
+            <button
+                onClick={toggleTheme}
+                className={`dark:bg-primary-bg bg-zinc-100 dark:text-primary-color text-zinc-500 border dark:border-zinc-800 border-zinc-200 rounded-full p-2 duration-300 transition-transform group: ${
+                  currentTheme === "light" ? "-rotate-180" : "rotate-0"
+                }`}
+                aria-label="Toggle Theme"
+              >
+                {currentTheme === "light" ? <SunIcon /> : <MoonIcon />}
+            </button>
             <MobileMenu />
           </div>
         </div>
